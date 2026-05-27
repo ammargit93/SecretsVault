99e51f3126b869323a22e1b4d23e4ad4053aff7d
Ammar Ansari <ammar.ice98@gmail.com>
2026-05-27 23:07:34 +0530

    (stable) persist kek and dek

--- middleware/secret.go
+++ middleware/secret.go
@@ -1,8 +1,14 @@
 package middleware
 
 import (
+	"crypto/rand"
+	"log"
+	"secretsvault/db"
+	"secretsvault/models"
+	"secretsvault/state"
 	"secretsvault/utils"
 	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -20,33 +26,29 @@ func WriteSecret(conn *pgxpool.Pool) fiber.Handler {
 		if err != nil {
 			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "invalid token"})
 		}
-
-		if err != nil {
-			return c.JSON(fiber.Map{"error": err})
-		}
 		if claims.ServiceRole == "RD" {
 			return c.JSON(fiber.Map{"Error": "Permission denied"})
 		} else {
-			// timestamp := time.Now().UTC().Format(time.RFC3339)
-			// KeyEncryptionKey, err := state.EncryptAES([]byte(timestamp), []byte(state.MasterKey))
-			// if err != nil {
-			// 	log.Fatalln("KEK generation failed")
-			// }
-			// kek := models.KeyEncryptionKey{
-			// 	KeyEncryptionKey: KeyEncryptionKey,
-			// 	Nonce:            []byte(rand.Text()),
-			// }
-			// db.InsertKEK(conn, kek)
+			timestamp := time.Now().UTC().Format(time.RFC3339)
+			KeyEncryptionKey, err := state.EncryptAES([]byte(timestamp), []byte(state.MasterKey))
+			if err != nil {
+				log.Fatalln("KEK generation failed")
+			}
+			kek := models.KeyEncryptionKey{
+				KeyEncryptionKey: KeyEncryptionKey,
+				Nonce:            []byte(rand.Text()),
+			}
+			db.InsertKEK(conn, kek)
 
-			// DataEncryptionKey, err := state.EncryptAES([]byte(timestamp), kek.KeyEncryptionKey)
-			// if err != nil {
-			// 	log.Fatalln("DEK generation failed")
-			// }
-			// dek := models.DataEncryptionKey{
-			// 	DataEncryptionKey: DataEncryptionKey,
-			// 	Nonce:             []byte(rand.Text()),
-			// }
-			// db.InsertDEK(conn, dek)
+			DataEncryptionKey, err := state.EncryptAES([]byte(timestamp), kek.KeyEncryptionKey)
+			if err != nil {
+				log.Fatalln("DEK generation failed")
+			}
+			dek := models.DataEncryptionKey{
+				DataEncryptionKey: DataEncryptionKey,
+				Nonce:             []byte(rand.Text()),
+			}
+			db.InsertDEK(conn, dek)
 		}
 		return c.JSON(fiber.Map{"claims": claims})
 	}
